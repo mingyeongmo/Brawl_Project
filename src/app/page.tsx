131c4484984cf4c775117fae38b7edff12b07737
민경모 <mingyeongmp@naver.com>
2024-09-23 22:44:44 +0900

    player api 추가 및 Promise all 을 통해 api 병렬호출

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,19 +12,33 @@ type ClubData = {
   members: Member[];
 }
 
+type PlayerData = {
+  name: string;
+  trophies: number;
+};
+
 export default function Home() {
   const [club, setClub] = useState<ClubData | undefined>();
+  const [player, setPlayer] = useState<PlayerData | undefined>();
+  
 
   useEffect(() => {
-    fetch("/api/player") // API Route 호출
-      .then((res) => res.json())
-      .then((data) => setClub(data))
+    // 두 개의 API를 병렬로 호출
+    Promise.all([
+      fetch("/api/club").then((res) => res.json()),
+      fetch("/api/player").then((res) => res.json())
+    ])
+      .then(([clubData, playerData]) => {
+        setClub(clubData);
+        setPlayer(playerData);
+      })
       .catch((error) => console.error("Error fetching club data:", error));
   }, []);
 
   if (!club) return <div>Loading...</div>;
 
   console.log("club : ", club);
+  console.log("club : ", player);
 
   const memberInfo = club.members.map(
     (member: { name: string; trophies: number }) => ({
@@ -45,6 +59,8 @@ export default function Home() {
           </li>
         ))}
       </ul>
+      <h1>{player?.name}</h1>
+      <p>{player?.trophies}</p>
     </div>
   );
 }
